4dec71f877fd056bed5ba41bbacd28f80a49f24b
jade.kim <jade.kim@lfin.kr>
2024-03-01 13:28:00 +0900

    CAL-11 더하기 구현 개발 완료

---
+++ test/src/main/java/com/wku/test/service/PlusFunction.java
@@ -0,0 +1,15 @@
+package com.wku.test.service;
+
+public class PlusFunction{
+
+
+    public int doPlust(Integer firstNumber, String word, Integer secondNumber){
+        Integer rtn = 0;
+
+        if(work == "+"){
+            rtn = firstNumber + secondNumber;
+        }
+
+        return rtn;
+    }
+}
